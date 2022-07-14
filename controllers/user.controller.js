02e1ccf49fee03f1c5864061ae8d679bf9af2908
devAkshay <akshay.jadhao@indicchain.com>
2022-07-14 18:40:55 +0530

    Transaction API structured

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -19,3 +19,32 @@ exports.createWallet = (req, res) => {
         appError.notFound(responseMessage.CREATE_WALLET_REQUIRED_MESSAGE);
     }
 }
+exports.transaction = async (req,res) =>{
+    const { newBalance, walletAddress } = req.body;
+    let userId = req.params.userId;
+    if(newBalance){
+        return res.status(400).json({message:"Enter the Balance"});
+    }
+    if(walletAddress){
+       return res.status(400).json({message:"Enter the Wallet Address"});
+    }
+    let user = await Wallet.findById({_id: userId})
+    if(user){
+        try {
+            let oldBalance = user.oldBalance;
+        let doc = new Wallet({
+            userId,
+            oldBalance,
+            newBalance,
+            walletAddress
+        });
+        doc.save();
+        res.status(200).json({status:"Success", message:"transaction successfully"})
+        } catch (error) {
+           res.status(400).json({status:"success",message:"Transaction Failed due to server error"});
+        }
+    }else{
+        return res.status(400).json({message:"User doesn't exists"})
+    }
+
+}
